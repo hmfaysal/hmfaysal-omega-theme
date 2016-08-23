06cc90c806aafcd7e7be0fa2e57f24a01c46cb50
Matija Čupić <matija.cupic.97@gmail.com>
2016-08-23 23:48:00 +0200

    assets formatting

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,12 +1,12 @@
 /*! Plugin options and other jQuery stuff */
 
 // FitVids options
-$(function() {
-	$("article").fitVids();
+$(function () {
+  $("article").fitVids();
 });
 
 // Table of Contents toggle
-$(function() {
+$(function () {
   $(".toc h3").click(function () {
     $("#drawer").toggleClass("hidden");
   });
@@ -16,14 +16,14 @@ $(function() {
 $("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
 // Magnific-Popup options
-$(document).ready(function() {
+$(document).ready(function () {
   $('.image-popup').magnificPopup({
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
     },
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
